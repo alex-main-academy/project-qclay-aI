8a6366dcdfbbb232eb4cb249c820ccc463287bd5
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-02-20 12:14:15 +0100

    work-with-list

--- src/components/pages/Services/components/Industry/Industry.jsx
+++ src/components/pages/Services/components/Industry/Industry.jsx
@@ -2,31 +2,14 @@ import css from './Industry.module.scss';
 import sprite from '../../images/sprite.svg';
 import figureImage from '../../images/industry/figure.png';
 import Media from 'react-media';
-import IndustryBlock from './IndustryBlock';
 import lineLottie from '../../lotties/lines_ai.json';
 import { Player } from '@lottiefiles/react-lottie-player';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
+import industryContentArray from './IndustryContentArray';
 
 const Industry = () => {
   const playerRef = useRef(Player);
-
-  const subtitlesArray = [
-    'Healthcare',
-    'Logistics',
-    'Real Estate',
-    'Education',
-    'Fintech',
-    'Agriculture',
-    'E-commerce',
-    'Gaming',
-    'Social Media',
-  ];
-
-  const [text, setText] = useState('Healthcare');
-
-  const handleChangeText = event => {
-    const title = event.target.innerText;
-    setText(title);
+  const handleToggleAnimation = () => {
     playerRef.current.stop();
     playerRef.current.play();
   };
@@ -40,19 +23,51 @@ const Industry = () => {
         </h2>
         <div className={css.industry__block}>
           <ul className={css.industry__list}>
-            {subtitlesArray.map(elem => {
+            {industryContentArray.map(({ title, content }) => {
               return (
                 <li
                   className={css.industry__item}
-                  key={elem}
-                  onMouseEnter={event => handleChangeText(event)}
+                  key={title}
+                  onMouseEnter={handleToggleAnimation}
                 >
                   <h3 className={css.industry__subtitle}>
-                    <span>{elem}</span>
+                    <span>{title}</span>
                     <svg width="37.6" height="25.6">
                       <use href={sprite + '#icon-arrow-bottom-right'}></use>
                     </svg>
                   </h3>
+                  <Media
+                    query="(max-width:768px)"
+                    render={() => (
+                      <div className={css.text__mobile}>
+                        <h3 className={css.mobile__title}>{title}</h3>
+                        {content.map(elem => {
+                          return (
+                            <p className={css.mobile__par} key={elem}>
+                              {elem}
+                            </p>
+                          );
+                        })}
+                      </div>
+                    )}
+                  />
+                  <Media
+                    query="(min-width:768px)"
+                    render={() => (
+                      <div className={css.industry__text}>
+                        <h3 className={css.text__title}>{title}</h3>
+                        <ul className={css.text__list}>
+                          {content.map(elem => {
+                            return (
+                              <li className={css.text__item} key={title}>
+                                <p className={css.text__par}>{elem}</p>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    )}
+                  />
                 </li>
               );
             })}
@@ -78,15 +93,6 @@ const Industry = () => {
                   className={css.industry__figure}
                 />
               </>
-            )}
-          />
-
-          <Media
-            query="(min-width:768px)"
-            render={() => (
-              <div className={css.industry__text}>
-                <IndustryBlock text={text} />
-              </div>
             )}
           />
         </div>
---
+++ src/components/pages/Services/components/Industry/IndustryContentArray.js
@@ -0,0 +1,79 @@
+const industryContentArray = [
+  {
+    id: '1',
+    title: 'Healthcare',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Provision of data analytics services to support healthcare decisions.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+  {
+    id: '2',
+    title: 'Logistics',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Provision of data analytics services to support healthcare decisions.',
+    ],
+  },
+  {
+    id: '3',
+    title: 'Real Estate',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+  {
+    id: '4',
+    title: 'Education',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Provision of data analytics services to support healthcare decisions.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+  {
+    id: '5',
+    title: 'Fintech',
+    content: [
+      '-Provision of data analytics services to support healthcare decisions.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+  {
+    id: '6',
+    title: 'Agriculture',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+  {
+    id: '7',
+    title: 'E-commerce',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Provision of data analytics services to support healthcare decisions.',
+    ],
+  },
+  {
+    id: '8',
+    title: 'Gaming',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Provision of data analytics services to support healthcare decisions.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+  {
+    id: '9',
+    title: 'Social Media',
+    content: [
+      '-Use of AI & automation to build functional & lifesaving AI systems in healthcare.',
+      '-Development of AI-enabled platforms for patient data collection to support diagnosis, notifications and disease prevention.',
+    ],
+  },
+];
+
+export default industryContentArray;
