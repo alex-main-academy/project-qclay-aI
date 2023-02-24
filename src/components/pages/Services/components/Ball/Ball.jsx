d67ad74fbfef6689dbd44fb0675ae630f0a4e285
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-02-24 16:08:01 +0100

    ball-video

--- src/components/pages/Services/components/Ball/Ball.jsx
+++ src/components/pages/Services/components/Ball/Ball.jsx
@@ -1,25 +1,12 @@
 import css from './Ball.module.scss';
-import ballImageMobile from '../../images/ball/ball-mobile.png';
-import Media from 'react-media';
 import lampVideo from '../../video/lamps.mp4';
 
 const Ball = () => {
   return (
     <section className={css.ball}>
-      <Media
-        query="(min-width:768px)"
-        render={() => (
-          <video autoPlay muted loop className={css.ball__video}>
-            <source src={lampVideo} />
-          </video>
-        )}
-      />
-      <Media
-        query="(max-width:767px)"
-        render={() => (
-          <img src={ballImageMobile} alt="ball" className={css.ball__image} />
-        )}
-      />
+      <video autoPlay muted loop className={css.ball__video}>
+        <source src={lampVideo} />
+      </video>
     </section>
   );
 };
