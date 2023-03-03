e7848090c9f13a16942fa2504a81dbd58e30e0e3
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-03-03 11:00:24 +0100

    change-tennis-v4

--- src/components/pages/Case/CasePage.jsx
+++ src/components/pages/Case/CasePage.jsx
@@ -1,9 +1,13 @@
 import CaseHero from './components/CaseHero/CaseHero';
+import Products from './components/Products/Products';
+import Works from './components/Works/Works';
 
 const Case = () => {
   return (
     <main>
       <CaseHero />
+      <Products />
+      <Works />
     </main>
   );
 };
